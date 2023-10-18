a411e4c7a9f1bb4f2b74fb17224aa0ae0f56dca3
Søren Døssing <netcom@sauber.net>
2023-10-18 09:51:13 +0000

    Wrapper for lazy loading

---
+++ src/repository/asset.ts
@@ -0,0 +1,14 @@
+import { JSONObject } from "/repository/repo.d.ts";
+
+/** Lazy load and cache result og callback */
+export class Asset {
+  private cache: null | JSONObject = null;
+
+  constructor(private readonly callback: () => Promise<JSONObject>) {}
+
+  /** Run callback and cache results */
+  async value(): Promise<JSONObject> {
+    if ( ! this.cache ) this.cache = await this.callback();
+    return this.cache;
+  }
+}
---
+++ src/repository/asset_test.ts
@@ -0,0 +1,11 @@
+import { Asset } from "./asset.ts";
+import { assertEquals } from "assert";
+import { JSONObject } from "./repo.d.ts";
+
+Deno.test("Blank callback", async () => {
+  const asset = new Asset(() =>
+    new Promise<JSONObject>((resolve) => resolve({}))
+  );
+  const result = await asset.value();
+  assertEquals(result, {});
+});
