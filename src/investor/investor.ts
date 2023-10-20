0cf61f9816ceb0ce367b778d615fedf1908ef9ec
Søren Døssing <netcom@sauber.net>
2023-10-20 19:33:55 +0000

    Assemble charts into one

--- src/investor/investor.ts
+++ src/investor/investor.ts
@@ -5,17 +5,19 @@ import { Stats } from "./stats.ts";
 import { Chart } from "./chart.ts";
 */
 
-import type { InvestorId } from "./mod.ts";
 import { Asset } from "/repository/mod.ts";
 import { ChartData, PortfolioData, StatsData } from "./mod.ts";
+import { ChartSeries } from "./chart-series.ts";
+import { Chart } from "./chart.ts";
+import { DateFormat } from "/utils/time/mod.ts";
 
- export class Investor {
+export class Investor {
   constructor(
     private readonly chartSeries: Asset<ChartData>,
     private readonly portfolioSeries: Asset<PortfolioData>,
-    private readonly statsSeries: Asset<StatsData>,
+    private readonly statsSeries: Asset<StatsData>
   ) {}
-/*
+  /*
   readonly portfolio: Portfolio;
   readonly stats: Stats;
   readonly chart: Chart;
@@ -25,5 +27,29 @@ import { ChartData, PortfolioData, StatsData } from "./mod.ts";
     this.stats = new Stats(repo, username, cid);
     this.chart = new Chart(repo, username, cid);
   }
-*/
+ */
+
+  /** Load chart and extract series */
+  private async series(date: DateFormat): Promise<ChartSeries> {
+    const data: ChartData = await this.chartSeries.value(date);
+    const chart: Chart = new Chart(data);
+    const series: ChartSeries = chart.series();
+    return series;
+  }
+
+  public async chartAssembly(): Promise<ChartSeries> {
+    const dates = await this.chartSeries.dates();
+    let series: ChartSeries = await this.series(dates[dates.length-1]);
+    let start: DateFormat = series.start();
+
+    // Prepend older charts
+    for (let i = dates.length - 2; i >= 0; i--) {
+      const date = dates[i];
+      if (i == 0 || (start && date >= start && dates[i - 1] < start)) {
+        series = series.combine(await this.series(date));
+        start = series.start();
+      }
+    }
+    return series;
+  }
 }
--- src/investor/investor_test.ts
+++ src/investor/investor_test.ts
@@ -1,16 +1,24 @@
-import { assertInstanceOf } from "assert";
+import { assertEquals, assertInstanceOf } from "assert";
 import { Investor } from "./investor.ts";
 import { ChartData } from "./chart.ts";
 import { PortfolioData } from "./portfolio.ts";
 import { StatsData } from "./stats.ts";
 import { repoBackend } from "/repository/testdata.ts";
 import { Asset } from "/repository/mod.ts";
+import { ChartSeries } from "./chart-series.ts";
 
-const charts = repoBackend.asset("chart") as Asset<ChartData>;
-const portfolios = repoBackend.asset("portfolio") as Asset<PortfolioData>;
-const stats = repoBackend.asset("stats") as Asset<StatsData>;
+const charts = repoBackend.asset("FundManagerZech.chart") as Asset<ChartData>;
+const portfolios = repoBackend.asset("FundManagerZech.portfolio") as Asset<PortfolioData>;
+const stats = repoBackend.asset("FundManagerZech.stats") as Asset<StatsData>;
 
 Deno.test("Blank Initialization", () => {
   const investor: Investor = new Investor(charts, portfolios, stats);
   assertInstanceOf(investor, Investor);
+});
+
+Deno.test("Chart Series", async () => {
+  const investor: Investor = new Investor(charts, portfolios, stats);
+  const series: ChartSeries = await investor.chartAssembly();
+  assertEquals(series.start(),  "2020-12-01");
+  assertEquals(series.values.length,  511);
 });
