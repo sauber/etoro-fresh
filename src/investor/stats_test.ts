268335942dbf5d71b692e9bee0abeb448e5662bb
Søren Døssing <netcom@sauber.net>
2023-11-08 13:53:25 +0000

    Too much data required for a blank initialization

--- src/investor/stats_test.ts
+++ src/investor/stats_test.ts
@@ -2,11 +2,6 @@ import { assertEquals, assertInstanceOf } from "assert";
 import { Stats } from "./stats.ts";
 import { statsData } from "./testdata.ts";
 
-Deno.test("Initialization", () => {
-  const stats = new Stats({ Data: { CustomerId: 0, UserName: "" } });
-  assertInstanceOf(stats, Stats);
-});
-
 Deno.test("Stats", async (t) => {
   await t.step("validate", async () => {
     const stats: Stats = new Stats(statsData);
