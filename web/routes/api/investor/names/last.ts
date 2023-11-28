0a8dd1469ebb4d81d25d72123ec19d1634953bab
Søren Døssing <netcom@sauber.net>
2023-11-28 08:54:08 +0000

    Reminder to only use last names

--- web/routes/api/investor/names/last.ts
+++ web/routes/api/investor/names/last.ts
@@ -4,6 +4,7 @@ import { Community, Names } from "/investor/mod.ts";
 export const handler: Handlers<null, { community: Community }> = {
   async GET(_req, ctx) {
     const community: Community = ctx.state.community;
+    // TODO, only return names from last dir
     const names: Names = await community.names();
     return new Response(JSON.stringify(names.values), {
       headers: { "Content-Type": "application/json" },
