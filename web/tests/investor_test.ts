be537ec291f20b9617d74dec7dd47ccc9b9754cc
Søren Døssing <netcom@sauber.net>
2023-11-29 12:44:45 +0000

    Correct path to names

--- web/tests/investor_test.ts
+++ web/tests/investor_test.ts
@@ -3,7 +3,7 @@ import manifest from "../fresh.gen.ts";
 import { assertEquals } from "$std/testing/asserts.ts";
 import type { DateFormat } from "/utils/time/mod.ts";
 import { assertInstanceOf } from "assert";
-import type { Names, InvestorExport } from "/investor/mod.ts";
+import type { InvestorExport, Names } from "/investor/mod.ts";
 
 const CONN_INFO: ServeHandlerInfo = {
   remoteAddr: { hostname: "127.0.0.1", port: 53496, transport: "tcp" },
@@ -15,7 +15,7 @@ Deno.test("HTTP assert test.", async (t) => {
   await t.step("#1 GET /api/investor/end", async () => {
     const resp = await handler(
       new Request("http://127.0.0.1/api/investor/end"),
-      CONN_INFO
+      CONN_INFO,
     );
     assertEquals(resp.status, 200);
     const date: DateFormat = await resp.json();
@@ -24,8 +24,8 @@ Deno.test("HTTP assert test.", async (t) => {
 
   await t.step("#2 GET /api/investor/names/last", async () => {
     const resp = await handler(
-      new Request("http://127.0.0.1/api/investor/names/last"),
-      CONN_INFO
+      new Request("http://127.0.0.1/api/investor/names"),
+      CONN_INFO,
     );
     assertEquals(resp.status, 200);
     const names: Names = await resp.json();
@@ -35,14 +35,12 @@ Deno.test("HTTP assert test.", async (t) => {
   await t.step("#3 GET /api/investor/FundManagerZech/last", async () => {
     const resp = await handler(
       new Request("http://127.0.0.1/api/investor/FundManagerZech/last"),
-      CONN_INFO
+      CONN_INFO,
     );
     assertEquals(resp.status, 200);
     const data = await resp.json() as InvestorExport;
     assertEquals(data.stats.UserName, "FundManagerZech");
   });
-
-
 
   /*
   await t.step("#2 POST /", async () => {
