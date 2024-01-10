fc418ccb968806d4b37f9e2f2827aa6f76c2203d
Søren Døssing <netcom@sauber.net>
2024-01-10 11:54:08 +0000

    Use refactored repo

--- src/scrape/refresh_bin.ts
+++ src/scrape/refresh_bin.ts
@@ -1,21 +1,21 @@
-import { RepoDiskBackend } from "/repository/repo-disk.ts";
+import { DiskBackend } from "/repository/disk-backend.ts";
 import { FetchWebBackend } from "./fetch-web.ts";
 import { Refresh } from "./refresh.ts";
-import { Config } from "../config/config.ts";
-import { Repo } from "/repository/repo.ts";
+import { Config } from "📚/config/config.ts";
+//import { Repo } from "/repository/repo.ts";
 import type { InvestorId } from "/investor/mod.ts";
 import type { DiscoverFilter } from "./mod.ts";
 
 const path: string = Deno.args[0];
-const backend: RepoDiskBackend = new RepoDiskBackend(path);
-const repo: Repo = new Repo(backend);
+const repo: DiskBackend = new DiskBackend(path);
+//const repo: Repo = new Repo(backend);
 
-const config: Config = repo.config;
+const config: Config = new Config(repo);
 const id = await config.get("investor") as InvestorId;
 const filter = await config.get("discover") as DiscoverFilter;
 const rate = await config.get("rate") as number;
 
 const fetcher: FetchWebBackend = new FetchWebBackend(rate);
-const refresh: Refresh = new Refresh(backend, fetcher, id, filter);
+const refresh: Refresh = new Refresh(repo, fetcher, id, filter);
 const count: number = await refresh.run();
 console.log(`Assets downloaded: ${count}`);
