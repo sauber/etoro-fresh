2bfd555d13517d2b287d4e322ad47f50f5d6efac
Søren Døssing <netcom@sauber.net>
2024-02-20 16:48:37 +0100

    rsi function

--- src/chart/indicators.ts
+++ src/chart/indicators.ts
@@ -14,7 +14,6 @@ export function sma(data: number[], window: number): number[] {
 
 /** Exponential Moving Average */
 export function ema(data: number[], window: number): number[] {
-
   const alpha = 2 / (window + 1);
   const emaValues = new Array(data.length).fill(NaN);
 
@@ -24,4 +23,41 @@ export function ema(data: number[], window: number): number[] {
   }
 
   return emaValues;
+}
+
+/** Relative Strength Index */
+export function rsi(data: number[], window: number): number[] {
+  const gains: number[] = [];
+  const losses: number[] = [];
+
+  for (let i = 1; i < data.length; i++) {
+    const difference = data[i] - data[i - 1];
+    if (difference > 0) {
+      gains.push(difference);
+      losses.push(0);
+    } else if (difference < 0) {
+      gains.push(0);
+      losses.push(-difference);
+    } else {
+      gains.push(0);
+      losses.push(0);
+    }
+  }
+  
+  const averageGain: number[] = ema(gains, window);
+  const averageLoss: number[] = ema(losses, window);
+  console.log({gains, losses, averageGain, averageLoss});
+
+  const rsiValues = new Array(data.length - window);
+
+  for (let i = 0; i < averageGain.length; i++) {
+    console.log(i, averageGain[i], averageLoss[i]);
+    if (averageLoss[i] === 0) rsiValues[i] = 100;
+    else {
+      const rs = averageGain[i] / averageLoss[i];
+      rsiValues[i] = 100 - 100 / (1 + rs);
+    }
+  }
+
+  return rsiValues;
 }
--- src/chart/indicators_test.ts
+++ src/chart/indicators_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "$std/assert/mod.ts";
-import { sma, ema } from "./indicators.ts";
+import { ema, rsi, sma } from "./indicators.ts";
 
 Deno.test("SMA", () => {
   const testData = [10, 20, 30, 40, 50, 60];
@@ -11,4 +11,18 @@ Deno.test("EMA", () => {
   const testData = [10, 20, 30, 40, 50, 60];
   const result = ema(testData, 3);
   assertEquals(result, [10, 15, 22.5, 31.25, 40.625, 50.3125]);
+});
+
+Deno.test("RSI", () => {
+  const testData = [40, 42, 44, 40, 38, 42, 45, 48, 50];
+  const windowSize = 5;
+
+  const result = rsi(testData, windowSize);
+  assertEquals(result, [
+    65,
+    76.76348547717842,
+    84.55172413793103,
+    88.4297520661157,
+    100
+  ]);
 });
