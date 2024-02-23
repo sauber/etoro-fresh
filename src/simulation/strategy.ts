8f9336b6a61863df70eedc6215781f8abe6775e2
Søren Døssing <netcom@sauber.net>
2024-02-23 10:58:08 +0100

    Describe strategy

--- src/simulation/strategy.ts
+++ src/simulation/strategy.ts
@@ -1,3 +1,28 @@
+/**
+ * Strategy takes at initialization a list of investors and the book.
+ * The open() method is called with a list of orders from previous strategies.
+ * Orders can be extended, trimmed or changed (amount).
+ * It returns the new list of orders.
+ * The close() method is called with a list of positions from previous strategies.
+ * Positions can be trimmed.
+ * It returns the new list os positions to close.
+ * Strategies can be chained.
+ * 
+ * Example:
+ *   const roll = new Strategy(investors, book)
+ *     .weekday('monday')
+ *     .unbox
+ *     .positions(40)
+ *     .crosspath(20,50);
+ * 
+ *   positions = roll.close(book.positions);
+ *   positions.forEach(p => p.close());
+ * 
+ *   orders = roll.open();
+ *   orders.forEach(o => o.open());
+ */
+
+
 import type { Investors } from "./mod.ts";
 import { Portfolio } from "./portfolio.ts";
 import type { Positions } from "./portfolio.ts";
