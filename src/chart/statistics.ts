210a3b3011fe279db7fffa66972950b2385467f5
Søren Døssing <netcom@sauber.net>
2024-02-26 09:58:23 +0100

    squared function

--- src/chart/statistics.ts
+++ src/chart/statistics.ts
@@ -12,20 +12,25 @@ export function avg(values: Numbers): number {
 
 /** Add a constant value to each number */
 export function add(values: Numbers, offset: number): Numbers {
-  return values.map(x => x+offset);
+  return values.map((x) => x + offset);
 }
 
 /** Raise each value to power */
 export function pow(values: Numbers, exponent: number): Numbers {
-  return values.map(x => Math.pow(x, exponent));
+  return values.map((x) => Math.pow(x, exponent));
+}
+
+/** number * number */
+export function squared(values: Numbers): Numbers {
+  return values.map((x) => x * x);
 }
 
 /** Standard Deviation */
 export function std(values: Numbers): number {
   const mean: number = avg(values);
   const variances: Numbers = add(values, -mean);
-  const squared: Numbers = pow(variances, 2);
-  const total: number = sum(squared);
-  const result: number = Math.sqrt(total / (values.length-1));
+  const sq: Numbers = squared(variances);
+  const total: number = sum(sq);
+  const result: number = Math.sqrt(total / (values.length - 1));
   return result;
 }
