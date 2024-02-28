864770ed7c9179bdc48728fbbf68ae11deaca49a
Søren Døssing <netcom@sauber.net>
2024-02-28 21:18:00 +0100

    Parabola regression

--- src/strategy/quadratic.ts
+++ src/strategy/quadratic.ts
@@ -1,4 +1,6 @@
+import { assertEquals } from "$std/assert/mod.ts";
 import { PolynomialRegression } from "npm:ml-regression-polynomial";
+import regression from "npm:regression";
 
 /** Perform quadratic polynomial regression and predict peak */
 export function peak(x: number[], y: number[]): { x: number; y: number } {
@@ -8,3 +10,27 @@ export function peak(x: number[], y: number[]): { x: number; y: number } {
   const peakY = regression.predict(peakX);
   return { x: peakX, y: peakY };
 }
+
+/** 
+ * Perform quadratic polynomial regression and predict peak.
+ * Find max y at
+ * Ref: https://tom-alexander.github.io/regression-js/
+ *   */
+export function rpeak(x: number[], y: number[]): { x: number; y: number } {
+  const data = x.map((x, i) => [x, y[i]]);
+
+  // var data = [[0,1],[32, 67] .... [12, 79]];
+  const pr = regression.polynomial(data, { order: 2, precision: 8 });
+  const coefficients: [number, number, number] = pr.equation;
+  const peakX: number = -coefficients[1] / (2 * coefficients[0]);
+  const peakY: number = pr.predict(peakX)[1];
+  //console.log({peakX, peakY});
+  return { x: peakX, y: peakY };
+}
+
+// Deno.test("Polynomial Regression", () => {
+//   const x = [0, 32, 12];
+//   const y = [1, 67, 79];
+//   const peak = rpeak(x, y);
+//   assertEquals(peak, 0);
+// })
--- src/strategy/quadratic_test.ts
+++ src/strategy/quadratic_test.ts
@@ -1,10 +1,19 @@
 import { assertAlmostEquals } from "$std/assert/mod.ts";
-import { peak } from "./quadratic.ts";
+import { peak, rpeak } from "./quadratic.ts";
 
 Deno.test("Quadratic Regression Peak", () => {
   const testData = [40, 42, 44, 40, 38, 42, 45, 48, 50];
   const x = [...Array(testData.length).keys()];
   const p = peak(x, testData);
+  assertAlmostEquals(p.x, 2.32, 0.1);
+  assertAlmostEquals(p.y, 40.30, 0.01);
+});
+
+Deno.test("Parabola Regression Peak", () => {
+  const testData = [40, 42, 44, 40, 38, 42, 45, 48, 50];
+  const x = [...Array(testData.length).keys()];
+  const p = rpeak(x, testData);
+  // console.log(p);
   assertAlmostEquals(p.x, 2.32, 0.1);
   assertAlmostEquals(p.y, 40.30, 0.01);
 });
