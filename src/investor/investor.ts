5373d5539c3dbc3412e38f1128da79568a289a11
Søren Døssing <netcom@sauber.net>
2024-04-11 13:10:27 +0200

    Library path

--- src/investor/investor.ts
+++ src/investor/investor.ts
@@ -1,7 +1,7 @@
 import { Chart } from "📚/chart/mod.ts";
 import { Diary } from "📚/investor/diary.ts";
-import type { StatsExport, InvestorId } from "../repository/mod.ts";
-import type { DateFormat } from "../time/mod.ts";
+import type { StatsExport, InvestorId } from "📚/repository/mod.ts";
+import type { DateFormat } from "📚/time/mod.ts";
 
 export type InvestorExport = {
   UserName: string;
