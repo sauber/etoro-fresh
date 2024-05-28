5d51fbd4e12d295ffc78933ee67c7287e1864b8c
Søren Døssing <netcom@sauber.net>
2024-05-28 20:51:42 +0200

    Scatter plot

--- src/neural/train.ts
+++ src/neural/train.ts
@@ -1,3 +1,5 @@
+import { plot } from "https://deno.land/x/chart/mod.ts";
+
 import { sum, Value } from "./value.ts";
 import { Network } from "./network.ts";
 
@@ -10,6 +12,9 @@ export class Train {
   private readonly lossHistory: number[] = [];
   private readonly xs: Values;
   private readonly ys: Values;
+  public epsilon = 0.0001;
+  public readonly beta = 0.9;
+  public velocityHistory = [0];
 
   constructor(
     private readonly network: Network,
@@ -31,7 +36,7 @@ export class Train {
     return mean;
   }
 
-    private step(n: number, learning_rate: number): void {
+  private step(n: number, learning_rate: number): void {
     const predict: Values = this.xs.map((line: Value[]) =>
       this.network.forward(line)
     );
@@ -45,16 +50,50 @@ export class Train {
     // loss.print();
 
     // Update
+
+    // Stochastic Gradient Descent
+    // velocity = 1;
+    // Gradient Descent with Momentum
+    const prev = this.velocityHistory[this.velocityHistory.length-1];
+    const velocity = this.beta * prev + ( 1 - this.beta) * loss.data;
+    this.velocityHistory.push(velocity);
+    // console.log('velocity', this.velocity);
+
     for (const p of this.network.parameters()) {
       p.data -= learning_rate * p.grad;
     }
 
-    console.log("Iteration", n, "loss", loss.data);
+    // console.log("Iteration", n, "loss", loss.data);
   }
 
   public run(iterations: number, rate: number = 0.1): void {
-    for (let i = 0; i < iterations; i++) {
+    let i = 0;
+    for (; i < iterations; i++) {
       this.step(i, rate);
+      // Step when loss is small enough
+      if (this.lossHistory[this.lossHistory.length - 1] < this.epsilon) break;
     }
+    console.log("Iterations: ", i);
   }
+
+  // Resample data to 80 columns and display ascii chart
+  private plot_graph(name: string, data: number[]): void {
+    const step = data.length / 80;
+    const samples: number[] = [];
+    for (let i = 0; i < data.length; i += step) {
+      samples.push(data[Math.floor(i)]);
+    }
+    console.log(name);
+    console.log(plot(samples, { height: 16 }));
+  }
+
+  /** Plot loss history */
+  public print_loss(): void {
+    this.plot_graph('Loss History', this.lossHistory);
+  }
+
+  public print_velocity(): void {
+    this.plot_graph('Velocity History', this.velocityHistory);
+  }
+
 }
--- src/neural/train_test.ts
+++ src/neural/train_test.ts
@@ -1,7 +1,8 @@
 import { assertInstanceOf } from "$std/assert/assert_instance_of.ts";
-import { Dense, Network, Relu, Sigmoid, Tanh, Binary } from "./network.ts";
+import { Dense, Network, Sigmoid, Relu } from "./network.ts";
 import { Inputs, Outputs, Train } from "./train.ts";
 import { Value } from "./value.ts";
+import { printImage } from "https://deno.land/x/terminal_images@3.1.0/mod.ts";
 
 // XOR training set
 const xs: Inputs = [
@@ -23,7 +24,10 @@ Deno.test("Initialize", () => {
   assertInstanceOf(train, Train);
 });
 
-Deno.test("Step", () => {
+// Dense(2), Sigmoid, Dense(1), Sigmoid is theoritically enough
+// but takes a long time to train, and often doesn't find the solution.
+// A much larger network using relu is faster and higher chance of success.
+Deno.test("XOR training", async () => {
   const network = new Network([
     new Dense(2, 9),
     new Relu(),
@@ -34,7 +38,7 @@ Deno.test("Step", () => {
   ]);
   const train = new Train(network, xs, ys);
   // console.log(train);
-  train.run(2000, 0.5);
+  train.run(200000, 0.9);
   // console.log(train);
 
   // Validate
@@ -45,5 +49,24 @@ Deno.test("Step", () => {
     )
   );
 
-  network.print();
+  // network.print();
+  train.print_loss();
+  // train.print_velocity();
+
+  // Generate scatter data
+  const size = 56;
+  const buffer: Array<number> = [];
+  for (let x = 0; x < size; ++x) {
+    for (let y = 0; y < size; ++y) {
+      const p = network.forward([new Value(x / (size-1)), new Value(y / (size-1))]);
+      const c = Math.floor(p[0].data * 256);
+      buffer.push(c, c, c, 255);
+    }
+  }
+  const imageBuffer: Uint8Array = new Uint8Array(buffer);
+
+  await printImage({
+    rawPixels: { width: size, height: size, data: imageBuffer },
+    width: 25,
+  });
 });
