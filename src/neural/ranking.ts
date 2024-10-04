bac90e18041afee7fec8b5479d6777f9d49d49c4
Søren Døssing <netcom@sauber.net>
2024-10-04 11:22:58 +0200

    Use Neurons lib.

--- src/neural/ranking.ts
+++ src/neural/ranking.ts
@@ -5,8 +5,10 @@ import { diffDate } from "📚/time/mod.ts";
 import { Features } from "📚/ranking/mod.ts";
 import { DataFrame } from "@dataframe";
 import { avg } from "📚/math/statistics.ts";
-import { Dense, LRelu, Network, Normalization, Relu } from "./micrograd.ts";
-import { Train } from "./train.ts";
+// import { Dense, LRelu, Network, Normalization, Relu } from "./micrograd.ts";
+import { Network, Train } from "jsr:@sauber/neurons@1.0.7";
+import { Dashboard } from "jsr:@sauber/ml-cli-dashboard";
+// import { Train } from "./train.ts";
 
 // Load investor data
 const backend = new Backend(Deno.args[0]);
@@ -36,53 +38,78 @@ const output = DataFrame.fromRecords(features.map((f) => f.output)).include([
   "SharpeRatio",
 ]);
 const corr = input.correlationMatrix(output);
-const columns = 5;
+const columns = 8;
 const weights = corr
   .amend("Abs", (r) => Math.abs(r.SharpeRatio as number))
   .sort("Abs")
   .reverse
   .slice(0, columns)
   .include(["Keys", "SharpeRatio"])
-  .rename({ Keys: "Key", SharpeRatio: "Weight" })
-  .bake;
+  .rename({ Keys: "Key", SharpeRatio: "Weight" });
 const w: Record<string, number> = Object.assign(
   {},
   ...weights.records.map((r) => ({ [r.Key as string]: r.Weight })),
 );
-console.log("Correlations:", w);
-const keys = weights.column("Key").values as string[];
+// console.log({weights}, weights.records);;
+// console.log("Correlations:", w);
+const keys = weights.values("Key") as string[];
+// console.log({keys});
 const inputs = input.include(keys);
 
 // Transform to training data
 const xs: number[][] = inputs.records.map((r) => Object.values(r) as number[]);
 const ys: number[][] = output.records.map((r) => [r.SharpeRatio as number]);
 
-const network = new Network([
-  new Normalization(5),
-  new Dense(5, 11),
-  new LRelu(),
-  new Dense(11, 5),
-  new LRelu(),
-  new Dense(5, 1),
-]);
+// const network = new Network([
+//   new Normalization(5),
+//   new Dense(5, 11),
+//   new LRelu(),
+//   new Dense(11, 5),
+//   new LRelu(),
+//   new Dense(5, 1),
+// ]);
+
+const network = new Network(columns).tanh.dense(11).lrelu.dense(5).lrelu.dense(1);
+
+// Mean number in list
+function mean(l: number[]): number {
+  return l.slice().sort()[Math.round(l.length/2)];
+}
+
+// Column from grid
+function col(g: number[][], index: number): number[] {
+  return g.map(row=>row[index]);
+}
+
+const means: number[] = xs[0].map((_,index) => mean(col(xs, index)));
+// console.log({means});
+
+function predict(a, b): number {
+  const input = means.slice();
+  input[0] = a;
+  input[1] = b;
+  const output = network.predict(input);
+  return output[0];
+}
+
+const scatter: Array<[number, number]> = xs.map(row=>[row[0], row[1]]);
+const values: Array<number> = ys.map(row=>row[0]);
+const dashboard = new Dashboard(78, 16, scatter, values, predict, 20000);
+
+function status (iterations: number, lossHistory: number[]): void {
+  const loss = lossHistory[lossHistory.length-1];
+  console.log(dashboard.render(iterations, loss));
+}
+
 const train = new Train(network, xs, ys);
 train.epsilon = 0.0001;
-train.run(200, 0.2);
-console.log(train.loss_chart());
+train.callback = status;
+train.callbackFrequency = 100;
+train.epsilon = 0.001;
+const iterations = train.run(20000, 0.1);
+// console.log(iterations);
 // network.print();
-
-// Scatter plot
-const xmin: number = Math.min(...xs.map((r) => r[0]));
-const xmax: number = Math.max(...xs.map((r) => r[0]));
-const ymin: number = Math.min(...xs.map((r) => r[1]));
-const ymax: number = Math.max(...xs.map((r) => r[1]));
-const pad: [number, number, number] = [
-  avg(xs.map((r) => r[2])),
-  avg(xs.map((r) => r[3])),
-  avg(xs.map((r) => r[4])),
-];
-
-await train.scatter_chart([xmin, xmax], [ymin, ymax], pad, 23);
+console.log(network.export);
 
 // Validation
 // xs.forEach((input, index) => console.log(input, ys[index], network.predict(input)));
